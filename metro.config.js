5e099f76862a12156785137bb30d0cc1853076fa
zeeshanayaz <zeeshanayaz1@gmail.com>
2025-05-23 19:38:18 +0500

    Added declaration for png and svg file type updated metro config to use svg files in app

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,12 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg');
+defaultConfig.resolver.sourceExts = [...defaultConfig.resolver.sourceExts, 'svg'];
+defaultConfig.transformer = {
+  ...defaultConfig.transformer,
+  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+};
+
+module.exports = mergeConfig(defaultConfig, {});
